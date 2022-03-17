4698fd0b5f063d58882be07fefbb6680dc5fae5d
Ivan Tyulkin <phantomlancerinyourmind@gmail.com>
2022-03-17 22:45:14 +0300

    init app

--- App.tsx
+++ App.tsx
@@ -1,20 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect } from "react";
+import { NativeBaseProvider } from "native-base";
+import RegistrationScreen from "./screens/RegistrationScreen";
 
 export default function App() {
+  useEffect(() => {
+    console.log('app mounted');
+  }, [])
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NativeBaseProvider>
+      <RegistrationScreen />
+    </NativeBaseProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ screens/RegistrationScreen.tsx
@@ -0,0 +1,30 @@
+import React, { useEffect } from "react";
+import { StatusBar } from 'expo-status-bar';
+import { Image, StyleSheet, Text, Vibration, View } from 'react-native';
+import { NativeBaseProvider, Box, Button, Icon, Input } from "native-base";
+import { Entypo } from "@native-base/icons";
+import RegistrationForm from "../components/RegistrationForm/RegistrationForm";
+
+
+const RegistrationScreen = () => {
+  const handlePressButton = () => {
+    Vibration.vibrate(1000);
+  }
+
+  return (
+      <View style={styles.container} >
+          <RegistrationForm />
+      </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+export default RegistrationScreen;
