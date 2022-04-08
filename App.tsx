2b21dcbe8e6f3de745e2035a50d172d4ef5ea8e3
Ivan Tyulkin <phantomlancerinyourmind@gmail.com>
2022-04-08 22:06:47 +0300

    [fix] replace files to src folder

--- App.tsx
+++ App.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { NativeBaseProvider } from 'native-base';
-import Router from './components/Router';
+import Router from './src/components/Router';
 
 export default function App() {
   useEffect(() => {
